ee69a0330b229798c5faff7833953ac0cb112cef
Alessandro Pacielli <alessandropacielli@gmail.com>
2021-05-10 22:35:40 +0200

    Refactor char-level syllabification

--- deepcomedy/models/transformer.py
+++ deepcomedy/models/transformer.py
@@ -206,3 +206,48 @@ class Transformer(tf.keras.Model):
         )  # (batch_size, tar_seq_len, target_vocab_size)
 
         return final_output, attention_weights
+
+
+###################################################################### Custom Schedule ###################################################################
+class TransformerCustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, d_model, warmup_steps=4000):
+        super(TransformerCustomSchedule, self).__init__()
+
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+####################################################################### Metrics ######################################################################
+def padded_loss(real, pred, loss_function):
+    """
+    Returns average loss excluding padded sequence elements.
+    """
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_function(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_sum(loss_) / tf.reduce_sum(mask)
+
+
+def padded_accuracy(real, pred):
+    """
+    Returns average accuracy excluding padded sequence elements.
+    """
+    accuracies = tf.equal(real, tf.cast(tf.argmax(pred, axis=2), dtype=tf.int32))
+
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    accuracies = tf.math.logical_and(mask, accuracies)
+
+    accuracies = tf.cast(accuracies, dtype=tf.float32)
+    mask = tf.cast(mask, dtype=tf.float32)
+    return tf.reduce_sum(accuracies) / tf.reduce_sum(mask)
